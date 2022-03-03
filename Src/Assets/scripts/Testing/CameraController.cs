ff2e3539f5b4760ef8d36166082e5eaeb4860756
yoongi3 <70143540+yoongi3@users.noreply.github.com>
2022-03-04 04:55:44 +1100

    summary
    smoother animation, camera controller, testing movement.

---
+++ Src/Assets/scripts/Testing/CameraController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] Vector3 offset;
+    [Range(1,10)]
+    [SerializeField] private float smoothFactor; 
+    [SerializeField] private Vector3 minValues, maxValues;
+
+    private void FixedUpdate(){
+        Follow();
+    }
+    void Follow(){
+        Vector3 targetPosition = player.position + offset;
+
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(targetPosition.x,minValues.x,maxValues.x),
+            Mathf.Clamp(targetPosition.y,minValues.y,maxValues.y),
+            Mathf.Clamp(targetPosition.z,minValues.z,maxValues.z));
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, smoothFactor * Time.fixedDeltaTime);
+        transform.position = smoothPosition;
+    }
+}
